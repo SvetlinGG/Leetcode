ce8f47352df091c321b2c7da3aa07c5484249537
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-07-19 17:06:31 +0200

    plus one final

--- arrays/plus-one.js
+++ arrays/plus-one.js
@@ -1,15 +1,10 @@
 function plusOne(nums){
 
    //let plus = nums.map(num => num + 1)
-   let newNum = Number(nums.join('')) + 1;
+   let newNum = BigInt(nums.join('')) + 1n;
    let arr = String(newNum).split('').map(num => Number(num));
-   console.log(arr);
-   
-   
-   
-    
-
+   return arr;
 
 }
-plusOne([1, 2, 3]);
-plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]);
+console.log(plusOne([1, 2, 3]));
+console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]));
