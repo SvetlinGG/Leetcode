f9abdc0924587a5f93f6bc72cffa7c497c8669b9
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-07-19 15:01:55 +0200

    two sum final

--- arrays/two-sum.js
+++ arrays/two-sum.js
@@ -1,6 +1,15 @@
-function twoSum(nums, raget){
+function twoSum(nums, target){
 
-    
+    for (let i = 0; i < nums.length; i++){
+        for (let j = i + 1; j < nums.length; j++){
+            if (nums[i] + nums[j] === target){
+                console.log([i, j]);
+            }
+        }
+    }
 
 }
-twoSum([2,7,11,15], 9)
+twoSum([2,7,11,15], 9);
+twoSum([3, 2, 4], 6);
+twoSum([3, 3], 6);
+twoSum([3, 2, 3], 6);
