78bb8326803070af04257240ef1a19f4f54ff775
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-07-20 23:12:35 +0200

    reduce transformation

--- arrays/array-reduce-transformation.js
+++ arrays/array-reduce-transformation.js
@@ -1,4 +1,13 @@
 function transformation(nums, fn, init){
     
+    let result = init;
+    for ( let i = 0; i < nums.length; i ++){
+
+        result = fn(result, nums[i])
+
+    }
+
+    return result;
 }
-transformation([1,2,3,4], function sum(accum, curr){return accum + curr}, 0)
+console.log(transformation([1,2,3,4], function sum(accum, curr){return accum + curr}, 0));
+console.log(transformation([1,2,3,4], function sum(accum, curr){return accum + curr * curr}, 100));
